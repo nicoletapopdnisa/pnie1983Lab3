8000485e7f86ff7f5551fbf4ff0955e6e9985a0f
nicoletapopdnisa <nicodeni.pop96@gmail.com>
2018-03-13 14:41:59 +0200

    Initial commit

---
+++ src/main/java/pnie1983/App.java
@@ -0,0 +1,13 @@
+package pnie1983;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
