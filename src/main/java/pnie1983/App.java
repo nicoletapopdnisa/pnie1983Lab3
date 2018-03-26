3b30e1c1f03fb536998cc2bd391916345178f834
nicoletapopdnisa <nicodeni.pop96@gmail.com>
2018-03-26 14:43:37 +0300

    Added lab2 maven project -  bugs fixed.

--- src/main/java/pnie1983/App.java
+++ src/main/java/pnie1983/App.java
@@ -4,10 +4,20 @@ package pnie1983;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import pnie1983.repository.Repository;
+import pnie1983.ui.DoctorUI;
+import pnie1983.controller.DoctorController;
+
+public class App {
+
+    public static void main(String[] args) {
+        String patients = "src/FilePatients.txt";
+        String consultations = "src/FileConsultations.txt";
+        Repository repo = new Repository(patients, consultations);
+        DoctorController ctrl = new DoctorController(repo);
+
+        DoctorUI console = new DoctorUI(ctrl);
+        console.Run();
     }
 }
